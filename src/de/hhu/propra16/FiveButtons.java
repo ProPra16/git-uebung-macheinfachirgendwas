46700c7f4dd82db6004b2ab94562e9d68c4cd73c
Marcel <makae103@hhu.de>
2016-06-14 13:34:02 +0200

    Added Button and Label

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -1,15 +1,23 @@
 package de.hhu.propra16;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtons extends Application {
+	
+	@FXML
+	private Button fileButton;
+	private Label fileLabel;
+	
     public static void main(String[] args) {
         launch(args);
     }
--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -4,4 +4,5 @@ package de.hhu.propra16;
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	
 }
