db3ca7365bc2ba6339e771f65240fc5f943e6776
lulu148 <pic18-7@pic18.acs.uni-duesseldorf.de>
2016-06-14 13:37:02 +0200

    created half of an Alert

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -1,16 +1,18 @@
 package de.hhu.propra16;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /**
  * Created by Tobias Witt on 06.06.16.
  */
-public class FiveButtons extends Application {
-    public static void main(String[] args) {
+public class FiveButtons extends Application {	
+	public static void main(String[] args) {
         launch(args);
     }
 
@@ -18,7 +20,7 @@ public class FiveButtons extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
-
+        
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
