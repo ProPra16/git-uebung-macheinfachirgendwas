d03d8374e94454f235fb35e9cc69da7318fd8def
Marcel <marcel.kaeufler@gmail.com>
2016-06-14 13:39:17 +0200

    Added onAction

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -4,5 +4,7 @@ package de.hhu.propra16;
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
-	
+	public void chooseFile(){
+		System.out.println("choose file");
+	}
 }
